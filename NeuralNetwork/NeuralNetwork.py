60daf63142b980b55fd5e6295ee8a20f95fb9a3f
Thomas Sonderman <trsonderm@gmail.com>
2016-12-15 15:22:03 -0500

    updated neural net

--- NeuralNetwork/NeuralNetwork.py
+++ NeuralNetwork/NeuralNetwork.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import optimize
 src = "http://iamtrask.github.io/2015/07/12/basic-python-network/"
 video = "https://www.youtube.com/watch?v=5u0jaA3qAGk"
 
@@ -46,7 +47,7 @@ class Neural_Network(object):
         self.yHat = self.forward(X)
         delta3 = np.multiply(-(y-self.yHat),self.sigmoidPrime(self.z3))
         dJdW2 = np.dot(self.a2.T, delta3)
-        delta2 = np.dot(delta3, self.W2.T)*self.sigmoidPrim(self.z2)
+        delta2 = np.dot(delta3, self.W2.T)*self.sigmoidPrime(self.z2)
         dJdW1 = np.dot(X.T, delta2)
 
         return dJdW1, dJdW2
@@ -63,9 +64,97 @@ class Neural_Network(object):
         W2_end = W1_end + self.hiddenLayerSize*self.outputLayerSize
         self.W2 = np.reshape(params[W1_end:W2_end], (self.hiddenLayerSize, self.outputLayerSize))
 
+    def computeGradients(self, X, y):
+        dJdW1, dJdW2 = self.costFunctionPrime(X, y)
+        return np.concatenate((dJdW1.ravel(), dJdW2.ravel()))
+
+
+def computeNumericalGradient(N, X, y):
+    paramsInitial = N.getParams()
+    numgrad = np.zeros(paramsInitial.shape)
+    perturb = np.zeros(paramsInitial.shape)
+    e = 1e-4
+
+    for p in range(len(paramsInitial)):
+        # Set perturbation vector
+        perturb[p] = e
+        N.setParams(paramsInitial + perturb)
+        loss2 = N.costFunction(X, y)
+
+        N.setParams(paramsInitial - perturb)
+        loss1 = N.costFunction(X, y)
+
+        # Compute Numerical Gradient
+        numgrad[p] = (loss2 - loss1) / (2 * e)
+
+        perturb[p] = 0
+
+    N.setParams(paramsInitial)
+
+    return numgrad
+
+class trainer(object):
+    def __init__(self, N):
+        # Make Local reference to network:
+        self.N = N
+
+    def callbackF(self, params):
+        self.N.setParams(params)
+        self.J.append(self.N.costFunction(self.X, self.y))
+
+    def costFunctionWrapper(self, params, X, y):
+        self.N.setParams(params)
+        cost = self.N.costFunction(X, y)
+        grad = self.N.computeGradients(X, y)
+        return cost, grad
+
+    def train(self, X, y):
+        # Make an internal variable for the callback function:
+        self.X = X
+        self.y = y
+
+        # Make empty list to store costs:
+        self.J = []
+
+        params0 = self.N.getParams()
+
+        options = {'maxiter': 200, 'disp': True}
+        _res = optimize.minimize(self.costFunctionWrapper, params0, jac=True, method='BFGS', \
+                                 args=(X, y), options=options, callback=self.callbackF)
+
+        self.N.setParams(_res.x)
+        self.optimizationResults = _res
+
 
 NN = Neural_Network()
+T = trainer(NN)
+T.train(X,y)
+NN.costFunctionPrime(X,y)
+NN.forward(X)
+#Test network for various combinations of sleep/study:
+hoursSleep = np.linspace(0, 10, 100)
+hoursStudy = np.linspace(0, 5, 100)
 
+#Normalize data (same way training data way normalized)
+hoursSleepNorm = hoursSleep/10.
+hoursStudyNorm = hoursStudy/5.
+
+#Create 2-d versions of input for plotting
+a, b  = np.meshgrid(hoursSleepNorm, hoursStudyNorm)
+
+#Join into a single input matrix:
+allInputs = np.zeros((a.size, 2))
+allInputs[:, 0] = a.ravel()
+allInputs[:, 1] = b.ravel()
+allOutputs = NN.forward(allInputs)
+yy = np.dot(hoursStudy.reshape(100,1), np.ones((1,100)))
+xx = np.dot(hoursSleep.reshape(100,1), np.ones((1,100))).T
+
+CS = plt.contour(xx,yy,100*allOutputs.reshape(100, 100))
+plt.clabel(CS, inline=1, fontsize=10)
+plt.xlabel('Hours Sleep')
+plt.ylabel('Hours Study')
+plt.show()
 yHat = NN.forward(X)
 
 plt.bar([0,1,2], y, width = 0.35, alpha=0.8)
